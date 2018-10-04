053bfce02beface1f02f7721d4a3c9354fcb76aa
MicahWeiss <micahjweiss@gmail.com>
2018-10-04 15:42:38 -0500

    VideoPlayer Container complete

--- src/containers/VideoPlayerContainer.js
+++ src/containers/VideoPlayerContainer.js
@@ -1,9 +1,28 @@
 import { connect } from 'react-redux';
 import VideoPlayer from '../components/VideoPlayer.js';
 
-var VideoPlayerContainer = () => {};
-
 //TODO: define a VideoPlayerContainer component which will hook up your action
 //dispatchers with your VideoPlayer component props.
+const mapStateToProps = (state) => { //gives data from store and turns it into props
+    return {
+        //needs current video
+        video: state.currentVideo
+    }
+}
+
+// NO ACTIONS HERE?????  
+// const mapDispatchToProps = (dispatch) => { // getting functions to action names.  
+//     return {  
+//     }
+// };
+
+var VideoPlayerContainer = connect(
+    mapStateToProps,
+    //mapDispatchToProps
+)(VideoPlayer)
+
+//TODO: define a VideoListContainer component which will hook up your action
+// dispatchers with your VideoList component props.
+
 
 export default VideoPlayerContainer;
