11317c7ead6c59d45782ae4ff08b93ef3ceccd2b
MicahWeiss <micahjweiss@gmail.com>
2018-10-04 11:40:03 -0500

    complete search action spec

--- src/actions/search.js
+++ src/actions/search.js
@@ -2,15 +2,37 @@ import searchYouTube from '../lib/searchYouTube.js';
 import changeVideoList from './videoList.js';
 import changeVideo from './currentVideo.js';
 import YOUTUBE_API_KEY from '../config/youtube.js';
-
+import thunk from 'redux-thunk';
 
 var handleVideoSearch = (q) => {
-  const SEND_QUERY = 'SEND_QUERY';
-  let options = {
-    key: YOUTUBE_API_KEY,
-    query: q
+  return () => {
+    searchYouTube({YOUTUBE_API_KEY, q},(data) => {
+      changeVideo(data);
+      changeVideoList(data);
+    })
   }
-  return searchYouTube(options, callback);
-};
+}
+
+// var handleVideoSearch = (q) => {
+//   const SEND_QUERY = 'SEND_QUERY';
+  
+  
+
+//   let options = {
+//     key: YOUTUBE_API_KEY,
+//     query: q
+//   }
+//   return searchYouTube(options, callback);
+// };
+
+// //from searchYouTube: callback(items)
+// var getSearchResults = (videos) => {
+//   const GET_VIDEOS_ARRAY = 'GET_VIDEOS_ARRAY';
+
+//   return {
+//     type: GET_VIDEOS_ARRAY,
+//     videos: videos
+//   }
+// }
 
 export default handleVideoSearch;
--- src/containers/VideoListContainer.js
+++ src/containers/VideoListContainer.js
@@ -2,7 +2,25 @@ import { connect } from 'react-redux';
 import VideoList from './../components/VideoList.js';
 import changeVideo from './../actions/currentVideo.js';
 
-var VideoListContainer = () => {};
+const mapStateToProps = (state) => {
+    return {
+        currentVideo: changeVideo(state.videos)
+    }
+}
+
+const handleVideoListEntryTitleClick = () => {
+    // console.log('dispatching cV', dispatch(changeVideo));
+    // return {
+    //     dispatch(changeVideo)
+    // }
+}
+
+
+
+var VideoListContainer = connect(
+    mapStateToProps,
+    handleVideoListEntryTitleClick
+)(VideoList)
 
 //TODO: define a VideoListContainer component which will hook up your action
 // dispatchers with your VideoList component props.
